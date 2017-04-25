f26329caa057c98c4eff420370fbe393ccf2202c
Micah Stubbs <micah.stubbs@gmail.com>
2017-04-25 10:21:54 -0400

    use window.innerHeight, tell eslint window is a global variable

--- 02/vis.js
+++ 02/vis.js
@@ -1,4 +1,4 @@
-/* global d3 */
+/* global d3 window */
 
 const container = d3.select('body');
 const article = d3.select('article');
@@ -10,7 +10,7 @@ const pane = [article, clone];
 let panes = 1;
 let divider = 0;
 
-var drag = d3.drag()
+const drag = d3.drag()
   .on('start', start)
   .on('drag', customDrag)
   .on('end', end);
@@ -19,7 +19,7 @@ console.log('drag', drag);
 article.call(drag);
 
 function start() {
-  const paneIndex = d3.event.y < divider * innerHeight ? 1 : 0;
+  const paneIndex = d3.event.y < divider * window.innerHeight ? 1 : 0;
   touches = touches.filter(d => d.alive || d.pane != paneIndex);
   touches.push({
     id: d3.event.identifier,
@@ -39,7 +39,7 @@ function customDrag() {
         pane[1].node().scrollTop = pane[0].node().scrollTop;
         pane[1].classed('alive', true);
       }
-      divider = ((live[0][1] + live[1][1]) / 2) / innerHeight;
+      divider = ((live[0][1] + live[1][1]) / 2) / window.innerHeight;
       pane[1].style('height', `${divider * 100}%`);
     } else if (panes !== 1) {
       panes = 1;
@@ -52,7 +52,7 @@ function customDrag() {
   if (pt) {
     pt.y = d3.event.y;
     pt.dy = d3.event.dy;
-    pt.pane = d3.event.y < divider * innerHeight ? 1 : 0;
+    pt.pane = d3.event.y < divider * window.innerHeight ? 1 : 0;
     pane[pt.pane].node().scrollTop -= pt.dy;
   }
 }
